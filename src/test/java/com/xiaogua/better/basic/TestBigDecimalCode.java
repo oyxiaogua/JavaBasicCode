689447f099e9b8f2f09de896a9a899411e17e471
mycg8199mail@163.com <mycg8199mail@163.com>
2016-02-20 19:10:33 +0800

    java基础代码

--- src/test/java/com/xiaogua/better/basic/TestBigDecimalCode.java
+++ src/test/java/com/xiaogua/better/basic/TestBigDecimalCode.java
@@ -1,6 +1,9 @@
 package com.xiaogua.better.basic;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,14 +11,31 @@ import org.junit.Test;
 public class TestBigDecimalCode {
 	@Test
 	public void testGetWithoutTrailingZeroes() {
-		//清除结尾多余的0
+		// 清除结尾多余的0
 		BigDecimal decimal = new BigDecimal("1.233300e3");
 		System.out.println(decimal.toString());
 		BigDecimal rtnDecimal = BigDecimalCode.getWithoutTrailingZeroes(decimal);
 		Assert.assertEquals("1233.3", rtnDecimal.toString());
-		
+
 		decimal = new BigDecimal("6e2");
 		rtnDecimal = BigDecimalCode.getWithoutTrailingZeroes(decimal);
 		Assert.assertEquals("600", rtnDecimal.toString());
 	}
+
+	@Test
+	public void testDecimalFormatCustomerStyle() {
+		BigDecimal decimal = new BigDecimal("1234567890987654321.01234567");
+		DecimalFormat formatter = (DecimalFormat) DecimalFormat.getInstance(Locale.ENGLISH);
+		DecimalFormatSymbols custom = new DecimalFormatSymbols();
+		custom.setDecimalSeparator('#');// 小数点
+		custom.setGroupingSeparator('$');// 整数分隔符
+		formatter.setGroupingSize(5);
+		formatter.setDecimalFormatSymbols(custom);
+		formatter.setMinimumIntegerDigits(1);
+		formatter.setMaximumIntegerDigits(10);
+		formatter.setMinimumFractionDigits(3);
+		formatter.setMaximumFractionDigits(5);
+		Assert.assertEquals("09876$54321#01235", formatter.format(decimal));
+	}
+
 }
