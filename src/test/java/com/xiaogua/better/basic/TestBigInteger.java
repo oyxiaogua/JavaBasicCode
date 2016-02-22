1f665cf8f6db2890e358a066cacc0debf387a362
mycg8199mail@163.com <mycg8199mail@163.com>
2016-02-22 19:38:36 +0800

    java基础代码

---
+++ src/test/java/com/xiaogua/better/basic/TestBigInteger.java
@@ -0,0 +1,17 @@
+package com.xiaogua.better.basic;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+public class TestBigInteger {
+
+	@Test
+	public void testBigIntegerDivideAndRemainder() {
+		BigInteger aBig = new BigInteger("5025");
+		BigInteger bBig = new BigInteger("1024");
+		BigInteger[] biArray = aBig.divideAndRemainder(bBig);
+		System.out.println("商:" + biArray[0]);
+		System.out.println("余数:" + biArray[1]);
+	}
+}
