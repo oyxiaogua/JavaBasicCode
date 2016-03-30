cd37e6a1f8be0b1c592d772e0868f59cd5494268
mycg8199mail@163.com <mycg8199mail@163.com>
2016-03-30 16:45:51 +0800

    commit

--- src/main/java/com/xiaogua/better/file/FileCommonUtil.java
+++ src/main/java/com/xiaogua/better/file/FileCommonUtil.java
@@ -3,7 +3,6 @@ package com.xiaogua.better.file;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
